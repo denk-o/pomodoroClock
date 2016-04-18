b9a86710304c9fc31858876e16afb23cb62167b3
Kevin Liu <kevinliu314@gmail.com>
2016-04-17 21:53:25 -0400

    selector functionality

--- scripts.js
+++ scripts.js
@@ -1,4 +1,38 @@
+var break_count = 5;
+var session_count = 25;
+
+
 $(document).ready(function(){
-  $("#break_count").html(5);
-  $("#session_count").html(25);
+  function updateFields(){
+    $("#break_count").html(break_count);
+    $("#session_count").html(session_count);
+  }
+
+  $(".down").on("click",function(){
+    if($(this).parents('.break_time').length){
+      if(break_count>1){
+        break_count=break_count-1;
+      }
+    }
+    else if($(this).parents('.session_time').length){
+      if(session_count>1){
+        session_count=session_count-1;
+      }
+    }
+
+    updateFields();
+  });
+
+  $(".up").on("click",function(){
+    if($(this).parents('.break_time').length){
+      break_count=break_count+1;
+    }
+    else if($(this).parents('.session_time').length){
+      session_count=session_count+1;
+    }
+
+    updateFields();
+  });
+
+  updateFields();
 });
